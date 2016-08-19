826a22c02715057d46d7514059b63b61c9918669
Casper LI <casper.hk@outlook.com>
2016-08-19 15:14:10 +0800

    Update InputValidator.php

--- php/InputValidator.php
+++ php/InputValidator.php
@@ -3,10 +3,10 @@
 
         // This method is used to check whether the username format is correct.
         // Reault
-        //      true (boolean) - The format is correct.
-        //         1     (int) - The length is less than the minimum.
-        //         2     (int) - The length is larger than the maximum.
-        //         3     (int) - The string is not only
+        //      true (Boolean) - The format is correct.
+        //         1 (Integer) - The length is less than the minimum.
+        //         2 (Integer) - The length is larger than the maximum.
+        //         3 (Integer) - The string is not only
         public static function isUsernameCorrect($username) {
             // Length checking.
             $minLength = 3;
@@ -22,23 +22,40 @@
 
         // This method is used to check whether the password format is correct.
         // Reault
-        //      true (boolean) - The format is correct.
-        //         1     (int) - The length is less than the minimum.
-        //         2     (int) - The length is larger than the maximum.
+        //      true (Boolean) - The format is correct.
+        //         1 (Integer) - The length is less than the minimum.
+        //         2 (Integer) - The length is larger than the maximum.
+        //         3 (Integer) - The string contain whitespace.
         public static function isPasswordCorrect($password) {
             // Length checking.
-            $minLength = 3;
-            $maxLength = 15;
+            $minLength = 6;
+            $maxLength = 20;
             if ($password < $minLength) { return 1; }
             if ($password > $maxLength) { return 2; }
+
+            // Check whether the password contains white space.
+            if (ctype_space($password)) { return 3; }
 
             return true;
         }
 
         // This method is used to check whether the email format is correct.
         // Reault
+        //      true (Boolean) - The format is correct.
+        //         1 (Integer) - The email does not contain '@'.
+        //         2 (Integer) - String before `@` is empty.
+        //         4 (Integer) - String after `@` is empty.
+        //         5 (Integer) - The email domain is blacklisted.
+        //         6 (Integer) - The email domain is not in the whitelist.
         public static function isEmailCorrect($email) {
+            $lengthOfEmail = strlen($email);
+            $pos = strpos($email, '@');
 
+            if ($pos == false) { return 1; }
+            if ($pos == 0) { return 2; }
+            if ($pos == ($lengthOfEmail-1)) { return 3; }
+
+            return true;
         }
 
         // This method is used to check whether the first name format is correct.
